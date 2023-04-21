868cff2df04a27dac151314d28bf43ff9fce02be
MarcelRyan <marcel.ryan2004@gmail.com>
2023-04-21 13:53:51 +0700

    feat: change main file for testing calculator temporary

--- src-backend/main.go
+++ src-backend/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "TUBES3_13521102/src-backend/FeatureDate"
+    "TUBES3_13521102/src-backend/FeatureCalculator"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    date := "25/02/2023"
+
+    dayName := FeatureDate.DateDayName(date)
+    fmt.Println(dayName)
+
+    hasil, err := FeatureCalculator.CalculateExpression("((3+5)/2.13)")
+
+    if (err != nil) {
+      fmt.Println(err)
+    } else {
+      fmt.Println(hasil)
+    }
 }
