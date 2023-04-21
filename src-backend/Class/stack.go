490b7c9dbedb854199ef4666d2240224155eac6f
MarcelRyan <marcel.ryan2004@gmail.com>
2023-04-21 13:52:34 +0700

    feat: add Stack class

---
+++ src-backend/Class/stack.go
@@ -0,0 +1,38 @@
+package Class
+
+import "fmt"
+
+type Stack struct {
+  // Stack that can contain any type of value
+  items []string
+}
+
+func (s* Stack) IsEmpty() bool {
+  return len(s.items) == 0
+}
+
+func (s* Stack) Size() int {
+  return len(s.items)
+}
+
+// Function to see the top of the stack
+func (s* Stack) Peek() (string, error) {
+  if (s.IsEmpty()) {
+    return "", fmt.Errorf("Invalid Syntax")
+  }
+  return s.items[len(s.items)-1], nil
+}
+
+func (s* Stack) Push(item string) {
+  s.items = append(s.items, item)
+}
+
+func (s* Stack) Pop() (string, error) {
+  if (s.IsEmpty()) {
+    return "", fmt.Errorf("Invalid Syntax")
+  }
+  item := s.items[len(s.items)-1]
+  // slicing the array
+  s.items = s.items[:len(s.items)-1]
+  return item, nil
+}
