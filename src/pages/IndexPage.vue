058f9204a8bc15e948b298a2c0843de849b15f56
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-22 17:01:55 +0700

    feat: base template

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,85 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <q-splitter v-model="splitter">
+      <template v-slot:before>
+        <div class="q-pa-md">
+          <div class="text-h4 q-mb-md">Before</div>
+          <div v-for="n in 20" :key="n" class="q-my-md">
+            {{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Quis praesentium cumque magnam odio iure quidem, quod illum numquam
+            possimus obcaecati commodi minima assumenda consectetur culpa fuga
+            nulla ullam. In, libero.
+          </div>
+        </div>
+      </template>
+
+      <template v-slot:after>
+        <div style="width: 100%" class="bg-red tw-h-screen">
+          <q-chat-message
+              v-if="isFetchingReponse"
+              name="BOT"
+              avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+              bg-color="amber"
+            >
+              <div>
+                Hi, how can i help you today ?
+              </div>
+            </q-chat-message>
+          <div v-for="message in messages" :key="message.getId()">
+            <q-chat-message
+              :sent="message.getStatus()"
+              label="John Doe"
+              stamp="4 minutes ago"
+              text-color="white"
+              bg-color="primary"
+            >
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/avatar.png" />
+              </q-avatar>
+              <div>{{ message.getText() }}</div>
+            </q-chat-message>
+
+            <q-chat-message
+              v-if="isFetchingReponse"
+              name="BOT"
+              avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+              bg-color="amber"
+            >
+              <q-spinner-dots size="2rem" />
+            </q-chat-message>
+          </div>
+
+          <q-input v-model="userInput" label="Type your message here" />
+          <q-btn label="Send" @click="sendMessage" />
+        </div>
+      </template>
+    </q-splitter>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+import { Message } from 'src/constants/message';
+import { ref, Ref, watch } from 'vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+const splitter = ref(20);
+
+const messages: Ref<Message[]> = ref([]);
+const userInput: Ref<string> = ref('');
+const isFetchingReponse: Ref<boolean> = ref(true);
+const sendMessage = () => {
+  const userMessage = new Message(
+    messages.value.length + 1,
+    true,
+    userInput.value
+  );
+  setTimeout(() => {
+    isFetchingReponse.value = false;
+  })
+  messages.value.push(userMessage);
+  userInput.value = '';
+};
+watch(userInput, () => {
+  isFetchingReponse.value = true;
+})
+
 </script>
