0ae806b8e57a2ecf06f9904432809414bb43f353
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-22 23:18:50 +0700

    feat: dummy response

---
+++ src/constants/history.ts
@@ -0,0 +1,22 @@
+export const chatTopic: string[] = [
+  "Topic - 1",
+  "Topic - 2",
+  "Topic - 3",
+  "Topic - 4",
+  "Topic - 5",
+  "Topic - 6",
+  "Topic - 7",
+  "Topic - 8",
+  "Topic - 9",
+];
+
+export const dummyResponse: string[] = [
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas sed tempus urna et pharetra pharetra.",
+  "Non pulvinar neque laoreet suspendisse interdum. Sed elementum tempus egestas sed. Cum sociis natoque penatibus et magnis dis parturient montes nascetur.",
+  "Fermentum leo vel orci porta non pulvinar. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Porttitor lacus luctus accumsan tortor. Semper risus in hendrerit gravida rutrum.",
+  "At elementum eu facilisis sed odio morbi quis commodo odio. Ultricies tristique nulla aliquet enim tortor. Faucibus et molestie ac feugiat.",
+  "Nisl pretium fusce id velit ut tortor pretium viverra suspendisse. Semper risus in hendrerit gravida rutrum quisque. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque.",
+  "Massa enim nec dui nunc mattis enim ut tellus elementum. Donec ultrices tincidunt arcu non sodales neque sodales ut.",
+  "Eget nunc scelerisque viverra mauris in aliquam sem fringilla. Tellus elementum sagittis vitae et leo duis ut diam.",
+];
