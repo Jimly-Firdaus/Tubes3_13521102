54baefd6144948dc67d41b843c69845824c4680f
MarcelRyan <marcel.ryan2004@gmail.com>
2023-04-23 15:53:06 +0700

    fix: resolve conflict

--- src-backend/main.go
+++ src-backend/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+    "database/sql"
     "fmt"
-    "TUBES3_13521102/src-backend/FeatureDate"
-    "TUBES3_13521102/src-backend/FeatureCalculator"
+    "log"
+    "TUBES3_13521102/src-backend/featureDate"
+    "TUBES3_13521102/src-backend/featureCalculator"
+    "TUBES3_13521102/src-backend/database"
+    _ "github.com/go-sql-driver/mysql"
+)
+
+// Variables to connect to database host
+const (
+  username = "root"
+  host = "containers-us-west-13.railway.app"
+  password = "PNGO6atNekbjjq4g2yPy"
+  port = "6330"
+  databasetype = "railway"
 )
 
 func main() {
@@ -12,11 +25,23 @@ func main() {
     dayName := FeatureDate.DateDayName(date)
     fmt.Println(dayName)
 
-    hasil, err := FeatureCalculator.CalculateExpression("((3+5)/2.13)")
+    hasil, err := FeatureCalculator.CalculateExpression("8 + 7 + 2+* 5")
 
     if (err != nil) {
       fmt.Println(err)
     } else {
       fmt.Println(hasil)
     }
+
+    db, err := sql.Open("mysql", database.ConnectDatabase(username, password, host, port, databasetype))
+    if (err != nil) {
+      fmt.Printf("Error %s while opening database\n", err)
+    }
+
+    pingErr := db.Ping()
+    if pingErr != nil {
+        log.Fatal(pingErr)
+    }
+    fmt.Println("Connected!")
+    defer db.Close()
 }
