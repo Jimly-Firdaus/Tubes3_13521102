680c20b236a4c5f7829f394294c4e1c8a68e9e93
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-23 23:59:23 +0700

    feat: interface for backend

---
+++ src/constants/message.d.ts
@@ -0,0 +1,36 @@
+// Payload from backend
+export interface Message {
+  id: number;
+  text: string;
+  response: string;
+  responseStatusCode: number;
+  sentTime: string;
+}
+
+export interface History {
+  historyId: number;
+  topic: string;
+  conversation: Array<Message>;
+}
+
+// Payload from backend
+export interface MessageHistory {
+  messageHistory: Array<History>;
+}
+
+// Request sent to backend on user-entered
+export interface Request {
+  message: Message;
+  method: "KMP" | "BoyerMoore";
+}
+
+// Future use --ignore this
+export interface userData {
+  userId: number;
+  username?: string;
+  messageHistory: MessageHistory;
+}
+
+export interface UserMessageHistory extends MessageHistory {
+  userId: number;
+}
