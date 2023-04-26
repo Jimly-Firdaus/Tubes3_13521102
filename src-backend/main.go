b648190f0689b3070377dba39668c37ce1ecde74
kenndave <dave.bahana@gmail.com>
2023-04-26 18:31:28 +0700

    add controllers for backend getting database

--- src-backend/main.go
+++ src-backend/main.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"TUBES3_13521102/src-backend/database"
-<<<<<<< HEAD
-=======
 	FeatureCalculator "TUBES3_13521102/src-backend/featureCalculator"
 	FeatureDate "TUBES3_13521102/src-backend/featureDate"
->>>>>>> 320028fe4c9951c4eab11fcf6c410eac3afd03dd
 	"TUBES3_13521102/src-backend/repository"
 	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -26,23 +21,25 @@ const (
   databasetype = "railway"
 )
 
-
 func main() {
+    date := "25/02/2023"
 
-  r := gin.Default()
-  // CORS middleware
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Next()
-	})
-  r.GET("/history", func(c *gin.Context) {
-    db, err := sql.Open("mysql", database.ConnectDatabase(username, password, host, port, databasetype))
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{
-            "error": err.Error(),
-        })
-        return
+    dayName := FeatureDate.DateDayName(date)
+    fmt.Println(dayName)
+
+    hasil, err := FeatureCalculator.CalculateExpression("((-8.05) + 3)")
+
+    if (err != nil) {
+      fmt.Println(err)
+    } else {
+      fmt.Println(hasil)
     }
+
+    db, err := sql.Open("mysql", database.ConnectDatabase(username, password, host, port, databasetype))
+    if (err != nil) {
+      fmt.Printf("Error %s while opening database\n", err)
+    }
+
     pingErr := db.Ping()
     if pingErr != nil {
         log.Fatal(pingErr)
@@ -50,35 +47,6 @@ func main() {
     fmt.Println("Connected!")
     defer db.Close()
 
-    historyList, err := repository.GetAllHistory(db)
-    c.JSON(http.StatusOK, gin.H{
-        "history": historyList,
-    })
-  })
-
-  r.GET("/botresponse", func(c *gin.Context) {
-    db, err := sql.Open("mysql", database.ConnectDatabase(username, password, host, port, databasetype))
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{
-            "error": err.Error(),
-        })
-        return
-    }
-    pingErr := db.Ping()
-    if pingErr != nil {
-        log.Fatal(pingErr)
-    }
-    defer db.Close()
-
-<<<<<<< HEAD
-    responseList, err := repository.GetAllBotResponse(db)
-
-    c.JSON(http.StatusOK, gin.H{
-        "botResponse": responseList,
-    })
-  })
-  r.Run(); // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-=======
     tes, err := repository.GetHistoryByHistoryID(db, 1)
 
     if err != nil {
@@ -94,31 +62,4 @@ func main() {
       fmt.Println(message.SentTime)
       fmt.Println(message.HistoryId)
     }
->>>>>>> 320028fe4c9951c4eab11fcf6c410eac3afd03dd
 }
-
-  // date := "25/02/2023"
-
-  //   dayName := FeatureDate.DateDayName(date)
-  //   fmt.Println(dayName)
-
-  //   hasil, err := FeatureCalculator.CalculateExpression("((-80.05) / (-1))")
-
-  //   if (err != nil) {
-  //     fmt.Println(err)
-  //   } else {
-  //     fmt.Println(hasil)
-  //   }
-
-  //   db, err := sql.Open("mysql", database.ConnectDatabase(username, password, host, port, databasetype))
-  //   if (err != nil) {
-  //     fmt.Printf("Error %s while opening database\n", err)
-  //   }
-
-  //   pingErr := db.Ping()
-  //   if pingErr != nil {
-  //       log.Fatal(pingErr)
-  //   }
-  //   fmt.Println("Connected!")
-  //   defer db.Close()
-
