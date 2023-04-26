afa9412d8e53e981e6977f47598a3bc154b214c6
MarcelRyan <marcel.ryan2004@gmail.com>
2023-04-26 16:01:01 +0700

    fix : change query functions because structs change

--- src-backend/repository/history.go
+++ src-backend/repository/history.go
@@ -5,40 +5,21 @@ import (
 	"database/sql"
 )
 
-func GetAllHistory(db *sql.DB) (results []structs.History, err error) {
-  // Getting data from MySQL
-  query := "SELECT * FROM History"
-
-  rows, err := db.Query(query)
-
-  if err != nil {
-    panic(err)
-  }
-
-  defer rows.Close()
-
-  for rows.Next() {
-    // Set History structs to be add to results
-    var history = structs.History{}
-    err = rows.Scan(&history.HistoryID, &history.HistoryTitle)
-    if err != nil {
-      panic(err)
-    }
-
-    results = append(results, history)
-  }
-
-  return
-}
-
 func InsertHistory(db *sql.DB, history structs.History) (error) {
-  errs := db.QueryRow("INSERT INTO History (historyTitle) VALUES ($1)", history.HistoryTitle)
+  errs := db.QueryRow("INSERT INTO History VALUES ($1, $2)", history.HistoryID, history.HistoryTitle)
 
   return errs.Err()
 }
 
 
 func DeleteHistory(db *sql.DB, history structs.History) (error) {
+  // Deleting from foreign key table first
+  _, errs := db.Exec("DELETE FROM HistoryMessage WHERE historyID = $1", history.HistoryID)
+
+  if errs != nil {
+    panic(errs)
+  }
+
   res, errs := db.Exec("DELETE FROM History WHERE historyID = $1", history.HistoryID)
   n, _ := res.RowsAffected()
 
@@ -68,8 +49,8 @@ func UpdateHistory(db *sql.DB, history structs.History) (error) {
   return nil
 }
 
-func GetHistoryByHistoryID(db *sql.DB, historyID int64) (results []structs.History, err error) {
-  rows, err := db.Query("SELECT * FROM History WHERE historyID = $1", historyID)
+func GetHistoryByHistoryID(db *sql.DB, historyID int64) (results structs.History, err error) {
+  rows, err := db.Query("SELECT * FROM History WHERE historyID = ?", historyID)
 
   if err != nil {
     panic(err)
@@ -78,15 +59,37 @@ func GetHistoryByHistoryID(db *sql.DB, historyID int64) (results []structs.Histo
   defer rows.Close()
 
   for rows.Next() {
-    var history = structs.History{}
 
-    err = rows.Scan(&history.HistoryID, &history.HistoryTitle)
+    err = rows.Scan(&results.HistoryID, &results.HistoryTitle)
 
     if err != nil {
       panic(err)
     }
 
-    results = append(results, history)
+    // Get all messages for current history
+    var questionId int64
+
+    messageRows, errs := db.Query("SELECT userQuestionID FROM HistoryMessage WHERE historyID = ?", results.HistoryID)
+
+    if errs != nil {
+      panic(errs)
+    }
+
+    for messageRows.Next() {
+      errs = messageRows.Scan(&questionId)
+
+      if errs != nil {
+        panic(errs)
+      }
+
+      message, err := GetUserMessageByID(db, questionId)
+
+      if err != nil {
+        panic(err)
+      }
+
+      results.Conversation = append(results.Conversation, message...)
+    }
   }
 
   return
---
+++ src-backend/repository/message.go
@@ -0,0 +1,56 @@
+package repository
+
+import (
+  "TUBES3_13521102/src-backend/structs"
+	"database/sql"
+)
+
+func InsertUserMessage(db *sql.DB, newMessage structs.Message) (error) {
+  err := db.QueryRow("INSERT INTO UserMessage(userQuestion, botAnswer, sentTime) VALUES(?, ?, ?)", newMessage.Text, newMessage.Response, newMessage.SentTime)
+
+  if err != nil {
+    return err.Err()
+  }
+
+  // Add to HistoryMessage table
+  err = db.QueryRow("INSERT INTO HistoryMessage VALUES(?, ?)", newMessage.HistoryId, newMessage.Id)
+
+  return err.Err()
+}
+
+
+func GetUserMessageByID(db *sql.DB, userQuestionID int64) (results []structs.Message, err error) {
+  rows, err := db.Query("SELECT * FROM UserMessage WHERE userQuestionID = ?", userQuestionID)
+
+  if err != nil {
+    panic(err)
+  }
+
+  defer rows.Close()
+
+  for rows.Next() {
+    // Set UserMessage structs to be added to results
+    var userMessage = structs.Message{}
+
+    err := rows.Scan(&userMessage.Id, &userMessage.Text, &userMessage.Response, &userMessage.SentTime)
+
+    if err != nil {
+      panic(err)
+    }
+
+    // Query to search for historyID
+    row, err := db.Query("SELECT historyID FROM HistoryMessage WHERE userQuestionID = ?", userMessage.Id)
+
+    if err != nil {
+      panic(err)
+    }
+
+    for row.Next() {
+      row.Scan(&userMessage.HistoryId);
+    }
+
+    results = append(results, userMessage)
+  }
+
+  return
+}
