d419e7fd71e44496cd9fa11e3348a46fe999a8bd
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-05-03 13:59:53 +0700

    fix: date format to match backend parser

--- src/composables/useUtility.ts
+++ src/composables/useUtility.ts
@@ -60,11 +60,11 @@ export const useUtility = ({
   const generateTimestamp = (): string => {
     const date = new Date();
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
 
     const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
     return formattedDate;
